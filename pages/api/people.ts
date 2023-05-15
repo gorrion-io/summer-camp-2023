df3bdfa56613a1595b874d313992cd450fe447d6
piotr-pajak <gloczek4lol@gmail.com>
2023-05-15 20:01:14 +0200

    Remove TODO comment

--- pages/api/people.ts
+++ pages/api/people.ts
@@ -18,13 +18,6 @@ type ErrorResponse = {
   readonly error: string;
 };
 
-/**
- * TODO: Prepare an endpoint to return a list of users
- * User faker.js or similar library to generate fake data, the minimal number of users is 100
- * The endpoint should return a pagination of 10 users per page
- * The endpoint should accept a query parameter "page" to return the corresponding page
- */
-
 function generateRandomUsers  (totalAmountOfUsers: number): User[] {
   const users: User[] = [];
   for (let i: number = 0; i < totalAmountOfUsers; i++) {
