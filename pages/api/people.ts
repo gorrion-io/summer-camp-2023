ed36d03300bf34c0137fc6fc6ba33e25b54d2213
piotr-pajak <gloczek4lol@gmail.com>
2023-05-15 19:52:06 +0200

    feat: Add pagination with error handling to people endpoint

--- pages/api/people.ts
+++ pages/api/people.ts
@@ -8,8 +8,14 @@ type User = {
   role: string;
 };
 
-type Response = {
- readonly data: User[];
+type Response<T> = {
+  readonly data: T;
+  readonly currentPage: number;
+  readonly totalAmountOfPages: number;
+};
+
+type ErrorResponse = {
+  readonly error: string;
 };
 
 /**
@@ -21,7 +27,7 @@ type Response = {
 
 function generateRandomUsers  (totalAmountOfUsers: number): User[] {
   const users: User[] = [];
-  for (let i:number = 0; i < totalAmountOfUsers; i++) {
+  for (let i: number = 0; i < totalAmountOfUsers; i++) {
     const user: User = {
       name: faker.person.fullName(),
       email: faker.internet.email(),
@@ -35,14 +41,30 @@ function generateRandomUsers  (totalAmountOfUsers: number): User[] {
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<User[]>
+  res: NextApiResponse<Response<User[]> | ErrorResponse>
 ): void {
-  const totalAmountOfUsers: number = 100
-  const users: User[] = generateRandomUsers(totalAmountOfUsers)
+  const { page = "1" } = req.query;
+  const usersAmountPerPage: number = 10;
+  const totalAmountOfUsers: number = 100;
+  const totalAmountOfPages: number = Math.ceil(totalAmountOfUsers / usersAmountPerPage);
 
-  const response: Response = {
-    data: users,
+  const currentPage: number = Number(page);
+  if (currentPage > totalAmountOfPages) {
+    res.status(400).json({ error: "Invalid page number" });
+    return;
   }
 
+  const startIndex: number = (currentPage - 1) * usersAmountPerPage;
+  const endIndex: number = startIndex + usersAmountPerPage;
+
+  const users: User[] = generateRandomUsers(totalAmountOfUsers)
+  const paginatedUsers: User[] = users.slice(startIndex, endIndex);
+
+  const response: Response<User[]> = {
+    data: paginatedUsers,
+    currentPage,
+    totalAmountOfPages
+  };
+
   res.status(200).json(response);
-}
+};
