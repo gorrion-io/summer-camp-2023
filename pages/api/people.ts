9299046fb2bf6c1fe3568d3dcbb5a33d3c18eae8
KeRoN00 <listwon.dev@gmail.com>
2023-05-17 14:54:55 +0200

    Created generateUsers function

--- pages/api/people.ts
+++ pages/api/people.ts
@@ -1,4 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { faker } from "@faker-js/faker";
+import { list } from "postcss";
 
 type User = {
   name: string;
@@ -14,9 +16,34 @@ type User = {
  * The endpoint should accept a query parameter "page" to return the corresponding page
  */
 
+const generateUsers = (count: number): User[] => {
+  const userList: User[] = [];
+
+  for (let i = 0; i < count; i++) {
+    const firstName: string = faker.person.firstName();
+    const lastName: string = faker.person.lastName();
+
+    const newUser: User = {
+      name: firstName + " " + lastName,
+      email: faker.internet.exampleEmail({
+        firstName: firstName,
+        lastName: lastName,
+      }),
+      title: faker.person.jobTitle(),
+      role: faker.person.jobType(),
+    };
+    userList.push(newUser);
+  }
+  
+  userList.sort((a,b) => a.name > b.name ? 1 : -1);
+  return userList;
+};
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<User[]>
 ) {
+  const { page = 1, count = 100 } = req.query;
+
   res.status(200).json([]);
 }
