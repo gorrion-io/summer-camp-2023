cc87fbfeee544d352e43ad0b57e73dd3a878aafd
mzajaczkowsky <mzajaczkowsky@gmail.com>
2023-05-26 06:01:40 +0200

    Prepare the pagination component.

--- pages/task.tsx
+++ pages/task.tsx
@@ -1,15 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
 export default function Task() {
-  /**  TODO: Create an endpoint that returns a list of people, and use that here.
-   * Use tanstack/react-query to fetch the data
-   */
-  const people = [
-    {
-      name: "Jane Cooper",
-      email: "jane@cooper.com",
-      title: "Regional Paradigm Technician",
-      role: "Admin",
-    },
-  ];
+  // set up the initial state for page
+  const [page, setPage] = useState(1);
+
+  // function which makes connection with api and returns a list of people along with additional values
+  async function getPeople(page = 1) {
+    const response = await fetch(
+      "http://localhost:3000/api/people?page=" + page
+    );
+    return response.json();
+  }
+
+  // fetching data with tanstack/react-query
+  const { data: people, isLoading } = useQuery({
+    queryKey: ["users", page],
+    queryFn: () => getPeople(page),
+  });
+
+  if (isLoading)
+    return (
+      <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+        Loading...
+      </div>
+    ); // waiting for async operation to complete before returning results
+
   return (
     <div className="mx-auto max-w-7xl">
       <div className="mt-8 flow-root">
@@ -48,7 +64,8 @@ export default function Task() {
                 </tr>
               </thead>
               <tbody className="divide-y divide-gray-800">
-                {people.map((person) => (
+                {/* showing users based on downloaded api data */}
+                {people.listOfUsers.map((person: any) => (
                   <tr key={person.email}>
                     <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">
                       {person.name}
@@ -73,24 +90,29 @@ export default function Task() {
             >
               <div className="hidden sm:block">
                 <p className="text-sm">
-                  Showing <span className="font-medium">1</span> to{" "}
-                  <span className="font-medium">1</span> of{" "}
-                  <span className="font-medium">N</span> results
+                  Showing{" "}
+                  <span className="font-medium">
+                    {(people.pageNumber - 1) * 10 + 1}
+                  </span>{" "}
+                  to <span className="font-medium">{people.dataRange}</span> of{" "}
+                  <span className="font-medium">{people.results}</span> results
                 </p>
               </div>
               <div className="flex flex-1 justify-between sm:justify-end">
-                <a
-                  href="#"
+                <button
+                  onClick={() => setPage((pageNumber) => pageNumber - 1)}
+                  disabled={page === 1}
                   className="relative inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0"
                 >
                   Previous
-                </a>
-                <a
-                  href="#"
+                </button>
+                <button
+                  onClick={() => setPage((pageNumber) => pageNumber + 1)}
+                  disabled={page === people.lastPage}
                   className="relative ml-3 inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0"
                 >
                   Next
-                </a>
+                </button>
               </div>
             </nav>
           </div>
